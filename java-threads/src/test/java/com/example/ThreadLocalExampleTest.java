67eb302147787d8f1421c6365c8706049a96f4c4
canattofilipe <canattofilipe@gmail.com>
2024-07-14 16:39:45 -0300

    InheritableThreadLocal example.

--- java-threads/src/test/java/com/example/ThreadLocalExampleTest.java
+++ java-threads/src/test/java/com/example/ThreadLocalExampleTest.java
@@ -128,6 +128,53 @@ public class ThreadLocalExampleTest {
 
     }
 
+    @Test
+    void InheritedThreadLocalTest() {
+
+        ThreadLocal<String> threadLocal = new ThreadLocal();
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("===== Thread 1 =====");
+            threadLocal.set("Thread 1 - ThreadLocal");
+            inheritableThreadLocal.set("Thread 1 - InheritableThreadLocal");
+
+            System.out.println(threadLocal.get());
+            System.out.println(inheritableThreadLocal.get());
+
+            Thread childThread = new Thread(() -> {
+                System.out.println("===== Child thread =====");
+                System.out.println(threadLocal.get());
+                System.out.println(inheritableThreadLocal.get());
+            });
+            childThread.start();
+
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                ;
+            }
+
+            System.out.println("===== Thread 2 =====");
+            System.out.println(threadLocal.get());
+            System.out.println(inheritableThreadLocal.get());
+
+        });
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     class MyObject {
 
     }
