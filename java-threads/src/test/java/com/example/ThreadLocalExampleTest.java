6e4e9ba64e9c13358108a9c26db0b9059722c0e2
canattofilipe <canattofilipe@gmail.com>
2024-07-15 12:43:05 -0300

    apply standards: name and fmt.

--- java-threads/src/test/java/com/example/ThreadLocalExampleTest.java
+++ java-threads/src/test/java/com/example/ThreadLocalExampleTest.java
@@ -4,178 +4,185 @@ import org.junit.jupiter.api.Test;
 
 public class ThreadLocalExampleTest {
 
-    @Test
-    void testThreadLocal() {
+  @Test
+  void threadLocalTest() {
 
-        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-        Thread t1 = new Thread(() -> {
-            threadLocal.set("Thread 1");
-            try {
+    Thread thread1 =
+        new Thread(
+            () -> {
+              threadLocal.set("Thread 1");
+              try {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {
+              } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-            final var value = threadLocal.get();
-            System.out.println("Value: " + value);
-        });
+              }
+              final var value = threadLocal.get();
+              System.out.println("Value: " + value);
+            });
 
-        Thread t2 = new Thread(() -> {
-            threadLocal.set("Thread 2");
-            try {
+    Thread thread2 =
+        new Thread(
+            () -> {
+              threadLocal.set("Thread 2");
+              try {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {
+              } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-            final var value = threadLocal.get();
-            System.out.println("Value: " + value);
-        });
+              }
+              final var value = threadLocal.get();
+              System.out.println("Value: " + value);
+            });
 
-        t1.start();
-        t2.start();
+    thread1.start();
+    thread2.start();
 
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    void testThreadLocalRemovingValue() {
+  @Test
+  void threadLocalRemovingValueTest() {
 
-        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-        Thread t1 = new Thread(() -> {
+    Thread thread1 =
+        new Thread(
+            () -> {
+              threadLocal.set("Thread 1");
+              var value = threadLocal.get();
+              System.out.println("Thread 1 value: " + value);
 
-            threadLocal.set("Thread 1");
-            var value = threadLocal.get();
-            System.out.println("Thread 1 value: " + value);
+              threadLocal.remove();
+              value = threadLocal.get();
+              System.out.println("Thread 1 value: " + value);
+            });
 
-            threadLocal.remove();
-            value = threadLocal.get();
-            System.out.println("Thread 1 value: " + value);
-        });
+    Thread thread2 =
+        new Thread(
+            () -> {
+              threadLocal.set("Thread 2");
+              var value = threadLocal.get();
+              System.out.println("Thread 2 value: " + value);
 
-        Thread t2 = new Thread(() -> {
-
-            threadLocal.set("Thread 2");
-            var value = threadLocal.get();
-            System.out.println("Thread 2 value: " + value);
-
-            try {
+              try {
                 Thread.sleep(3000);
-            } catch (InterruptedException e) {
+              } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+              }
 
-            value = threadLocal.get();
-            System.out.println("Thread 2 value: " + value);
+              value = threadLocal.get();
+              System.out.println("Thread 2 value: " + value);
 
-            threadLocal.remove();
-            value = threadLocal.get();
-            System.out.println("Thread 2 value: " + value);
-        });
+              threadLocal.remove();
+              value = threadLocal.get();
+              System.out.println("Thread 2 value: " + value);
+            });
 
-        t1.start();
-        t2.start();
+    thread1.start();
+    thread2.start();
 
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    void testThreadLocalWithInitialValue() {
+  @Test
+  void threadLocalWithInitialValueTest() {
 
-        ThreadLocal<MyObject> threadLocal1 = new ThreadLocal<>() {
-            @Override
-            protected MyObject initialValue() {
-                return new MyObject();
-            }
+    ThreadLocal<MyObject> threadLocal1 =
+        new ThreadLocal<>() {
+          @Override
+          protected MyObject initialValue() {
+            return new MyObject();
+          }
         };
 
-        ThreadLocal<MyObject> threadLocal2 = ThreadLocal.withInitial(MyObject::new);
+    ThreadLocal<MyObject> threadLocal2 = ThreadLocal.withInitial(MyObject::new);
 
-
-        Thread t1 = new Thread(() -> {
-            System.out.println("Thread 1: " + threadLocal1.get());
-            System.out.println("Thread 1: " + threadLocal2.get());
-        });
-
-        Thread t2 = new Thread(() -> {
-            System.out.println("Thread 2: " + threadLocal1.get());
-            System.out.println("Thread 2: " + threadLocal2.get());
-        });
-
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    @Test
-    void InheritedThreadLocalTest() {
-
-        ThreadLocal<String> threadLocal = new ThreadLocal();
-        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
-
-        Thread thread1 = new Thread(() -> {
-            System.out.println("===== Thread 1 =====");
-            threadLocal.set("Thread 1 - ThreadLocal");
-            inheritableThreadLocal.set("Thread 1 - InheritableThreadLocal");
-
-            System.out.println(threadLocal.get());
-            System.out.println(inheritableThreadLocal.get());
-
-            Thread childThread = new Thread(() -> {
-                System.out.println("===== Child thread =====");
-                System.out.println(threadLocal.get());
-                System.out.println(inheritableThreadLocal.get());
+    Thread thread1 =
+        new Thread(
+            () -> {
+              System.out.println("Thread 1: " + threadLocal1.get());
+              System.out.println("Thread 1: " + threadLocal2.get());
             });
-            childThread.start();
 
-        });
-        thread1.start();
+    Thread thread2 =
+        new Thread(
+            () -> {
+              System.out.println("Thread 2: " + threadLocal1.get());
+              System.out.println("Thread 2: " + threadLocal2.get());
+            });
 
-        Thread thread2 = new Thread(() -> {
-            try {
+    thread1.start();
+    thread2.start();
+
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void InheritedThreadLocalTest() {
+
+    ThreadLocal<String> threadLocal = new ThreadLocal();
+    InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+
+    Thread thread1 =
+        new Thread(
+            () -> {
+              System.out.println("===== Thread 1 =====");
+              threadLocal.set("Thread 1 - ThreadLocal");
+              inheritableThreadLocal.set("Thread 1 - InheritableThreadLocal");
+
+              System.out.println(threadLocal.get());
+              System.out.println(inheritableThreadLocal.get());
+
+              Thread childThread =
+                  new Thread(
+                      () -> {
+                        System.out.println("===== Child thread =====");
+                        System.out.println(threadLocal.get());
+                        System.out.println(inheritableThreadLocal.get());
+                      });
+              childThread.start();
+            });
+    thread1.start();
+
+    Thread thread2 =
+        new Thread(
+            () -> {
+              try {
                 Thread.sleep(3000);
-            } catch (InterruptedException e) {
+              } catch (InterruptedException e) {
                 e.printStackTrace();
                 ;
-            }
+              }
 
-            System.out.println("===== Thread 2 =====");
-            System.out.println(threadLocal.get());
-            System.out.println(inheritableThreadLocal.get());
+              System.out.println("===== Thread 2 =====");
+              System.out.println(threadLocal.get());
+              System.out.println(inheritableThreadLocal.get());
+            });
+    thread2.start();
 
-        });
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    class MyObject {
-
-    }
+  class MyObject {}
 }
