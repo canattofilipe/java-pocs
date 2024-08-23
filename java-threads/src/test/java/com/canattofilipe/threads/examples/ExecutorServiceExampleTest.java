5dee0757e8de4f67198caa662ebdf0ce66d9f555
canattofilipe <canatto.filipe@gmail.com>
2024-08-23 11:28:05 -0300

    Executor Service 2.

--- java-threads/src/test/java/com/canattofilipe/threads/examples/ExecutorServiceExampleTest.java
+++ java-threads/src/test/java/com/canattofilipe/threads/examples/ExecutorServiceExampleTest.java
@@ -1,10 +1,11 @@
 package com.canattofilipe.threads.examples;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,7 +13,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import org.junit.jupiter.api.Test;
 
-public class ExecutorServiceExample1Test {
+public class ExecutorServiceExampleTest {
 
   @Test
   void simpleExecServiceWithRunnableAndVoidReturn() {
@@ -82,6 +83,54 @@ public class ExecutorServiceExample1Test {
 
     assertTrue(executorService.isShutdown());
     assertTrue(future.isDone());
+  }
+
+  @Test
+  void shuttingDownExamples() {
+
+    // shutdown() waits for all tasks to finish
+    ExecutorService executorService1 = Executors.newFixedThreadPool(1);
+    executorService1.shutdown();
+    assertTrue(executorService1.isShutdown());
+
+    // shutdownNow() interrupts all running tasks
+    ExecutorService executorService2 = Executors.newFixedThreadPool(1);
+    for (int i = 0; i < 10; i++) {
+      executorService2.execute(newRunnable("Task " + i));
+    }
+    List<Runnable> runnables = executorService2.shutdownNow();
+    assertTrue(executorService2.isShutdown());
+    System.out.println("[runnables]: " + runnables.size());
+    assertTrue(runnables.size() > 0);
+
+    // awaitTermination() waits for all tasks to finish or timeout
+    ExecutorService executorService3 = Executors.newFixedThreadPool(1);
+    for (int i = 0; i < 10; i++) {
+      executorService3.execute(newRunnable("Task " + i));
+    }
+    try {
+      final var result =
+          executorService3.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS);
+      System.out.println("[awaitTermination]: " + result);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void cancellingTaskViaFutureTest() {
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    final var future = executorService.submit(newCallable("Task 1"));
+    assertFalse(future.isDone());
+    try {
+      final var wasCancelled = future.cancel(true);
+      assertTrue(wasCancelled);
+      future.get();
+    } catch (CancellationException e) {
+      System.out.println("Cannot get result from cancelled task");
+    } catch (Exception e) {
+      fail("Should not reach here");
+    }
   }
 
   private static Runnable newRunnable(String msg) {
